8cf59cb6ff117e32ed86ec2c0b1cc0152310906b
Joseph <joseph@good-idea.studio>
2024-01-09 10:29:50 -0800

    chore: set up & apply prettier config

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 'xgbrv2vi',
-    dataset: 'production'
-  }
-})
+    dataset: 'production',
+  },
+});
--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,7 +1,7 @@
-import {defineConfig} from 'sanity'
-import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
-import {schemaTypes} from './schemas'
+import { defineConfig } from 'sanity';
+import { deskTool } from 'sanity/desk';
+import { visionTool } from '@sanity/vision';
+import { schemaTypes } from './schemas';
 
 export default defineConfig({
   name: 'default',
@@ -15,4 +15,4 @@ export default defineConfig({
   schema: {
     types: schemaTypes,
   },
-})
+});
