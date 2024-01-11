2d96a37fc946eb1fdfaa62844c203cfae4a053f3
Bryan Washington <beewash92@gmail.com>
2024-01-11 14:05:37 -0500

    style: add screen breakpoints and brand colors to tailwind config

--- www/tailwind.config.ts
+++ www/tailwind.config.ts
@@ -7,13 +7,28 @@ const config: Config = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-      },
+    screens: {
+      sm: '320px',
+      md: '600px',
+      lg: '1200px',
+      xl: '1920px',
     },
+    colors: {
+      black: '#131D2B',
+      white: '#FFFFFF',
+      grey: '#F3F5F4',
+      'grey-medium': '#E6E8E7',
+      'grey-dark': '#CACCCB',
+      'grey-darker': '#575958',
+      sienna: '#7D320C',
+      'sienna-dark': '#521B00',
+      midnight: '#131D2B',
+      'midnight-light': '#29374A',
+      sky: '#ABD9EA',
+      yellow: '#FFD714',
+      'yellow-light': '#FEFFD6',
+    },
+    extend: {},
   },
   plugins: [],
 };
