8e27af1a30410c12aea261f2564d5b50d4c1a1ff
Bryan Washington <beewash92@gmail.com>
2024-01-23 17:16:32 -0500

    chore: add NavLink file

---
+++ www/src/components/Navigation/NavLink.tsx
@@ -0,0 +1,126 @@
+import { FC, useRef, useState, useEffect } from 'react';
+import Link from 'next/link';
+import cn from 'classnames';
+import { useUIProvider } from '@/context/UIProvider';
+import { SimpleIcon } from '@/svgs/SimpleIcon';
+import * as Types from '@/types/sanity';
+import {
+  NavLinkDropdownWrapper,
+  NavDropdownButton,
+  NavLinkDropdown,
+  SubPageLink,
+  NavLinkStyles,
+} from './styles';
+
+type Props = {
+  navItem: Types.NavGroup;
+  isMobile?: boolean;
+};
+
+export const NavLink: FC<Props> = ({ navItem, isMobile }) => {
+  const {
+    mobileNavOpen,
+    toggleGlobalNav,
+    currentNavItemRef,
+    setCurrentNavItemRef,
+  } = useUIProvider();
+
+  const navItemRef = useRef<HTMLDivElement>(null);
+  const dropdownRef = useRef<HTMLDivElement>(null);
+  const [dropdownOpen, setDropdownOpen] = useState<boolean>(false);
+  const parentSlug = navItem.link?.slug;
+  const navItemHighlightState =
+    currentNavItemRef === null
+      ? 'text-black'
+      : currentNavItemRef === navItemRef.current
+        ? 'text-black'
+        : 'text-black/60';
+
+  const hoverToggleDropdownOpen = () => {
+    setDropdownOpen(true);
+  };
+
+  const hoverToggleDropdownClose = () => {
+    setDropdownOpen(false);
+  };
+
+  useEffect(() => {
+    if (!navItemRef.current) return;
+    if (!dropdownRef.current) return;
+
+    if (isMobile) {
+      if (currentNavItemRef === navItemRef.current && !dropdownOpen) {
+        setCurrentNavItemRef(null);
+      }
+      // close dropdown if mobile nav is closed
+      if (!mobileNavOpen) {
+        setDropdownOpen(false);
+      }
+    }
+
+    // hover events for desktop nav
+    navItemRef.current.addEventListener('mouseover', () => {
+      setCurrentNavItemRef(navItemRef.current);
+    });
+
+    dropdownRef.current.addEventListener('mouseover', () => {
+      hoverToggleDropdownOpen();
+    });
+
+    dropdownRef.current.addEventListener('mouseleave', () => {
+      hoverToggleDropdownClose();
+    });
+  }, [currentNavItemRef, dropdownRef, mobileNavOpen, dropdownOpen]);
+  return (
+    <div
+      ref={navItemRef}
+      className={cn(NavLinkStyles, `${navItem.label}`, navItemHighlightState)}
+    >
+      {navItem.subpages ? (
+        <div ref={dropdownRef} className={cn(NavLinkDropdownWrapper)}>
+          <button
+            className={cn(NavDropdownButton)}
+            onClick={() => {
+              if (isMobile) {
+                setCurrentNavItemRef(navItemRef.current);
+                setDropdownOpen(!dropdownOpen);
+              }
+            }}
+          >
+            <p className={cn(NavLink)}>{navItem.label}</p>
+            <SimpleIcon
+              type="arrow-down"
+              className={cn(
+                dropdownOpen ? 'rotate-90' : '',
+                'transition ease-in-out w-3',
+                navItemHighlightState,
+              )}
+            />
+          </button>
+          <div
+            className={cn(
+              NavLinkDropdown,
+              dropdownOpen ? 'block' : 'hidden',
+              'transition-all ease-in-out',
+            )}
+          >
+            {navItem.subpages.map((subPage) => (
+              <Link
+                key={subPage._key}
+                href={`${parentSlug}/${subPage.link.slug}`}
+                className={cn(SubPageLink)}
+                onClick={toggleGlobalNav}
+              >
+                {subPage.label}
+              </Link>
+            ))}
+          </div>
+        </div>
+      ) : (
+        <Link href={navItem.link.slug} onClick={toggleGlobalNav}>
+          {navItem.label}
+        </Link>
+      )}
+    </div>
+  );
+};
