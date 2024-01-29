843f9e6fb78f7541c800ed123ac5ec565f49bacc
Joseph <joseph@good-idea.studio>
2024-01-29 10:14:41 -0800

    style: add desktop footer styles

--- www/src/components/Footer/index.tsx
+++ www/src/components/Footer/index.tsx
@@ -4,13 +4,17 @@ import { Footer as FooterType } from '@/types/sanity';
 import { filterMaybes } from '@/utils/arrays';
 import { Link } from '@/atoms/Link';
 import { CTA } from '@/components/CTA';
+import { LogotypeColor } from '@/svgs/Logotype';
 import {
   FooterBottomLinks,
   FooterBottomNavGroup,
   FooterCta,
+  FooterLogotype,
+  FooterMain,
   FooterNavGroup,
   FooterNavGroups,
   FooterOuter,
+  FooterQRCode,
 } from './styles';
 
 type FooterProps = {
@@ -24,28 +28,38 @@ export const Footer: FC<FooterProps> = ({ footer }) => {
       <div className={cn(FooterCta)}>
         <CTA cta={mobileCta} />
       </div>
-      <div className={cn(FooterNavGroups)}>
-        {filterMaybes(footerNavGroups).map((footerGroup) => (
-          <div className={cn(FooterNavGroup)} key={footerGroup._key}>
-            {filterMaybes(footerGroup.navItems).map((navItem) => (
-              <div key={navItem._key} className="font-size-8">
-                <Link link={navItem.link}>{navItem.label}</Link>
+      <div className={cn(FooterMain)}>
+        <div className="flex-grow">
+          <div className={cn(FooterLogotype)}>
+            <LogotypeColor />
+          </div>
+          <div className={cn(FooterNavGroups)}>
+            {filterMaybes(footerNavGroups).map((footerGroup) => (
+              <div className={cn(FooterNavGroup)} key={footerGroup._key}>
+                {filterMaybes(footerGroup.navItems).map((navItem) => (
+                  <div key={navItem._key} className="font-size-8">
+                    <Link link={navItem.link}>{navItem.label}</Link>
+                  </div>
+                ))}
               </div>
             ))}
           </div>
-        ))}
+        </div>
+        <div className={cn(FooterQRCode)}>
+          <span>QR Code placeholder</span>
+        </div>
       </div>
       <div className={cn(FooterBottomLinks)}>
         <div className={cn(FooterBottomNavGroup)}>
           {filterMaybes(bottomLinks?.leftLinks).map((navItem) => (
-            <div key={navItem._key} className="font-size-10">
+            <div key={navItem._key} className="font-size-10 md:mr-8">
               <Link link={navItem.link}>{navItem.label}</Link>
             </div>
           ))}
         </div>
         <div className={cn(FooterBottomNavGroup)}>
           {filterMaybes(bottomLinks?.rightLinks).map((navItem) => (
-            <div key={navItem._key} className="font-size-10">
+            <div key={navItem._key} className="font-size-10 md:ml-8">
               <Link link={navItem.link}>{navItem.label}</Link>
             </div>
           ))}
