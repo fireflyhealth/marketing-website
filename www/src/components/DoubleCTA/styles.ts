daad5580bf8c433ca115f8632fa9d9e6f5952a3d
Bryan Washington <beewash92@gmail.com>
2024-02-09 11:18:03 -0500

    chore: remove unused styles

--- www/src/components/DoubleCTA/styles.ts
+++ www/src/components/DoubleCTA/styles.ts
@@ -1,12 +1,3 @@
-export const Wrapper = [
-  'relative w-full pt-8 p-4 pb-[127px]',
-  'md:absolute md:top-auto md:p-12',
-];
-
-export const BackgroundColor = [
-  'absolute top-0 left-0 z-navigationCTAGradient w-full h-full bg-yellow-linear-gradient backdrop-blur-[75px]',
-];
-
 export const CTA_Container = [
   'flex flex-col space-y-3',
   'md:flex-row md:space-y-0 md:space-x-12',
