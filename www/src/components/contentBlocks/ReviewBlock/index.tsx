2a65d42307dddd66d2fcc4d60771d13e4b4af763
Bryan Washington <beewash92@gmail.com>
2024-02-16 10:31:30 -0500

    feat: add load more button

--- www/src/components/contentBlocks/ReviewBlock/index.tsx
+++ www/src/components/contentBlocks/ReviewBlock/index.tsx
@@ -1,6 +1,7 @@
-import { FC } from 'react';
+import { FC, useState, useEffect } from 'react';
 import cn from 'classnames';
 import { ReviewBlock as ReviewBlockType } from '@/types/sanity';
+import { Button } from '@/atoms/Button';
 import { ContentBlockHeader } from '../ContentBlockWrapper';
 import { ReviewItem } from './ReviewItem';
 import { Wrapper, Header, Reviews } from './styles';
@@ -11,6 +12,32 @@ type Props = {
 
 export const ReviewBlock: FC<Props> = ({ reviewBlock }) => {
   const { header, reviews } = reviewBlock;
+
+  const [isLoadMoreVisible, setIsLoadMoreVisible] = useState(true);
+  // State handling the number of reviews to show at a time
+  const [reviewListIndex, setReviewListIndex] = useState(3);
+
+  // Slice reviews so it's length = reviewListIndex
+  const initialReviewList = reviews.slice(0, reviewListIndex);
+
+  const handleLoadMore = () => {
+    setReviewListIndex(reviewListIndex + 6);
+  };
+
+  // only show the load more button if there are more than 3 reviews
+  useEffect(() => {
+    if (reviews.length > 3) {
+      setIsLoadMoreVisible(true);
+    } else setIsLoadMoreVisible(false);
+  }, [reviews]);
+
+  // Hide 'Load more' button when all reviews are shown
+  useEffect(() => {
+    if (reviewListIndex >= reviews.length) {
+      setIsLoadMoreVisible(false);
+    }
+  }, [reviewListIndex, reviews.length]);
+
   return (
     <div className={cn(Wrapper)}>
       {header && (
@@ -22,9 +49,18 @@ export const ReviewBlock: FC<Props> = ({ reviewBlock }) => {
         </div>
       )}
       <div className={cn(Reviews)}>
-        {reviews.map((review) => (
+        {initialReviewList.map((review) => (
           <ReviewItem key={review._key} reviewItem={review} />
         ))}
+        {isLoadMoreVisible && (
+          <Button
+            id=""
+            label="Load more"
+            onClick={handleLoadMore}
+            width="auto"
+            align="left"
+          />
+        )}
       </div>
     </div>
   );
