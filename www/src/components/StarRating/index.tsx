3b458bc2a3fc17574dc5e9ae2926b3a30a5b04b5
Bryan Washington <beewash92@gmail.com>
2024-02-22 10:21:40 -0500

    feat: refactor star rating component

--- www/src/components/StarRating/index.tsx
+++ www/src/components/StarRating/index.tsx
@@ -1,5 +1,5 @@
 import { FC } from 'react';
-import StyledRating from '@mui/material/Rating';
+import cn from 'classnames';
 import { SimpleIcon } from '@/svgs/SimpleIcon';
 
 type Props = {
@@ -9,15 +9,43 @@ type Props = {
 export const StarRating: FC<Props> = ({ starRating }) => {
   return (
     <div>
-      <StyledRating
-        name="customized-color"
-        defaultValue={starRating}
-        icon={<SimpleIcon type="star" wrapperStyles="w-3 text-black" />}
-        emptyIcon={
-          <SimpleIcon type="star" wrapperStyles="w-3 text-grey-dark" />
-        }
-        readOnly
-      />
+      <div className="flex flex-row space-x-1">
+        <SimpleIcon
+          type="star"
+          wrapperStyles={cn(
+            starRating >= 1 ? 'text-black' : 'text-grey-dark',
+            'w-3',
+          )}
+        />
+        <SimpleIcon
+          type="star"
+          wrapperStyles={cn(
+            starRating >= 2 ? 'text-black' : 'text-grey-dark',
+            'w-3',
+          )}
+        />
+        <SimpleIcon
+          type="star"
+          wrapperStyles={cn(
+            starRating >= 3 ? 'text-black' : 'text-grey-dark',
+            'w-3',
+          )}
+        />
+        <SimpleIcon
+          type="star"
+          wrapperStyles={cn(
+            starRating >= 4 ? 'text-black' : 'text-grey-dark',
+            'w-3',
+          )}
+        />
+        <SimpleIcon
+          type="star"
+          wrapperStyles={cn(
+            starRating >= 5 ? 'text-black' : 'text-grey-dark',
+            'w-3',
+          )}
+        />
+      </div>
     </div>
   );
 };
