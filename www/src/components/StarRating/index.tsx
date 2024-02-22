3c7db4236f03d4c123857323d1bf6233ba70c841
Bryan Washington <beewash92@gmail.com>
2024-02-22 10:26:04 -0500

    feat: remove starRating style sheet

--- www/src/components/StarRating/index.tsx
+++ www/src/components/StarRating/index.tsx
@@ -8,44 +8,42 @@ type Props = {
 
 export const StarRating: FC<Props> = ({ starRating }) => {
   return (
-    <div>
-      <div className="flex flex-row space-x-1">
-        <SimpleIcon
-          type="star"
-          wrapperStyles={cn(
-            starRating >= 1 ? 'text-black' : 'text-grey-dark',
-            'w-3',
-          )}
-        />
-        <SimpleIcon
-          type="star"
-          wrapperStyles={cn(
-            starRating >= 2 ? 'text-black' : 'text-grey-dark',
-            'w-3',
-          )}
-        />
-        <SimpleIcon
-          type="star"
-          wrapperStyles={cn(
-            starRating >= 3 ? 'text-black' : 'text-grey-dark',
-            'w-3',
-          )}
-        />
-        <SimpleIcon
-          type="star"
-          wrapperStyles={cn(
-            starRating >= 4 ? 'text-black' : 'text-grey-dark',
-            'w-3',
-          )}
-        />
-        <SimpleIcon
-          type="star"
-          wrapperStyles={cn(
-            starRating >= 5 ? 'text-black' : 'text-grey-dark',
-            'w-3',
-          )}
-        />
-      </div>
+    <div className="flex flex-row space-x-1">
+      <SimpleIcon
+        type="star"
+        wrapperStyles={cn(
+          starRating >= 1 ? 'text-black' : 'text-grey-dark',
+          'w-3',
+        )}
+      />
+      <SimpleIcon
+        type="star"
+        wrapperStyles={cn(
+          starRating >= 2 ? 'text-black' : 'text-grey-dark',
+          'w-3',
+        )}
+      />
+      <SimpleIcon
+        type="star"
+        wrapperStyles={cn(
+          starRating >= 3 ? 'text-black' : 'text-grey-dark',
+          'w-3',
+        )}
+      />
+      <SimpleIcon
+        type="star"
+        wrapperStyles={cn(
+          starRating >= 4 ? 'text-black' : 'text-grey-dark',
+          'w-3',
+        )}
+      />
+      <SimpleIcon
+        type="star"
+        wrapperStyles={cn(
+          starRating >= 5 ? 'text-black' : 'text-grey-dark',
+          'w-3',
+        )}
+      />
     </div>
   );
 };
