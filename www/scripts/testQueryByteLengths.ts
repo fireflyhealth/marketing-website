f7cf1210c3170af3cc79f58dfa26321919508027
Joseph <joseph@good-idea.studio>
2024-02-23 13:58:15 -0700

    ci: test sanity page query byte lengths

---
+++ www/scripts/testQueryByteLengths.ts
@@ -0,0 +1,54 @@
+/* eslint-disable no-console */
+import { getByteLength } from '@/utils/getByteLength';
+import * as queries from '../src/lib/sanity/queries';
+
+const SANITY_MAX = 307200;
+
+type Result = { queryName: string; byteLength: number };
+
+/**
+ * Ensure that our page queries do not exceed the sanity client maximum.
+ * (This will cause builds to fail).
+ * See more: https://twist.com/a/133876/ch/675943/t/5809345/c/88554381
+ */
+
+const main = () => {
+  console.log(
+    `Checking that sanity queries to not exceed the byte maximum of ${SANITY_MAX}`,
+  );
+  const results = Object.entries(queries).reduce<Result[]>(
+    (prev, [key, query]) => [
+      ...prev,
+      {
+        queryName: key,
+        byteLength: getByteLength(query),
+      },
+    ],
+    [],
+  );
+
+  results.forEach(({ queryName, byteLength }) => {
+    const symbol =
+      byteLength > SANITY_MAX
+        ? '🔴'
+        : byteLength > SANITY_MAX * 0.85
+          ? '🟡'
+          : '🟢';
+    const percent = Math.round((byteLength / SANITY_MAX) * 100)
+      .toString()
+      .padStart(2, '0')
+      .concat('%');
+    console.log(`${symbol} ${queryName}: ${byteLength} (${percent})`);
+  });
+
+  const tooLargeResults = results.filter((r) => r.byteLength > SANITY_MAX);
+
+  if (tooLargeResults.length) {
+    const tooLargeNames = tooLargeResults.map((r) => r.queryName).join(', ');
+    throw new Error(
+      `One or more queries exceed the maximum byte length: ${tooLargeNames}`,
+    );
+  }
+};
+
+main();
