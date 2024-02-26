20ce6855cc23290c0c4412a8335aa7962bfd50c6
Sohee Lim <limsohee1002@gmail.com>
2024-02-26 15:40:35 -0500

    fix: fix dropdown nav bugfix

--- www/src/components/NavCTA/styles.ts
+++ www/src/components/NavCTA/styles.ts
@@ -1,8 +1,8 @@
 export const Wrapper = [
-  'relative w-full pt-8 p-4 pb-[127px]',
-  'md:absolute md:top-auto md:p-12',
+  'relative w-full pt-8 p-4 pb-[96px]',
+  'md:absolute md:top-auto md:p-12 md:pb-[128px]',
 ];
 
 export const BackgroundColor = [
-  'absolute top-0 left-0 z-navigationCTAGradient w-full h-full bg-yellow-linear-gradient backdrop-blur-[75px]',
+  'absolute top-0 left-0 z-navigationCTAGradient w-full h-full bg-yellow-linear-gradient',
 ];
