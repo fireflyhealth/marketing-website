84edc1b9ee96f3da1eb348e51371bcf330a2c5a6
Bryan Washington <beewash92@gmail.com>
2024-02-29 11:55:16 -0500

    feat: wrap footer in theme component

--- www/src/components/Footer/index.tsx
+++ www/src/components/Footer/index.tsx
@@ -5,6 +5,7 @@ import { filterMaybes } from '@/utils/arrays';
 import { Link } from '@/atoms/Link';
 import { CTA } from '@/components/CTA';
 import { LogotypeColor } from '@/svgs/Logotype';
+import { Theme, ColorTheme } from '../Theme';
 import {
   FooterBottomLinks,
   FooterBottomNavGroup,
@@ -25,49 +26,51 @@ type FooterProps = {
 export const Footer: FC<FooterProps> = ({ footer }) => {
   const { mobileCta, footerNavGroups, bottomLinks } = footer;
   return (
-    <footer className={cn(FooterOuter)}>
-      <div className={cn(FooterInner)}>
-        <div className={cn(FooterCta)}>
-          <CTA cta={mobileCta} />
-        </div>
-        <div className={cn(FooterMain)}>
-          <div className="flex-grow">
-            <div className={cn(FooterLogotype)}>
-              <LogotypeColor />
+    <Theme theme={ColorTheme.Grey}>
+      <footer className={cn(FooterOuter)}>
+        <div className={cn(FooterInner)}>
+          <div className={cn(FooterCta)}>
+            <CTA cta={mobileCta} />
+          </div>
+          <div className={cn(FooterMain)}>
+            <div className="flex-grow">
+              <div className={cn(FooterLogotype)}>
+                <LogotypeColor />
+              </div>
+              <div className={cn(FooterNavGroups)}>
+                {filterMaybes(footerNavGroups).map((footerGroup) => (
+                  <div className={cn(FooterNavGroup)} key={footerGroup._key}>
+                    {filterMaybes(footerGroup.navItems).map((navItem) => (
+                      <div key={navItem._key} className="font-size-8">
+                        <Link link={navItem.link}>{navItem.label}</Link>
+                      </div>
+                    ))}
+                  </div>
+                ))}
+              </div>
             </div>
-            <div className={cn(FooterNavGroups)}>
-              {filterMaybes(footerNavGroups).map((footerGroup) => (
-                <div className={cn(FooterNavGroup)} key={footerGroup._key}>
-                  {filterMaybes(footerGroup.navItems).map((navItem) => (
-                    <div key={navItem._key} className="font-size-8">
-                      <Link link={navItem.link}>{navItem.label}</Link>
-                    </div>
-                  ))}
+            <div className={cn(FooterQRCode)}>
+              <span>QR Code placeholder</span>
+            </div>
+          </div>
+          <div className={cn(FooterBottomLinks)}>
+            <div className={cn(FooterBottomNavGroup)}>
+              {filterMaybes(bottomLinks?.leftLinks).map((navItem) => (
+                <div key={navItem._key} className="font-size-10 md:mr-8">
+                  <Link link={navItem.link}>{navItem.label}</Link>
+                </div>
+              ))}
+            </div>
+            <div className={cn(FooterBottomNavGroup)}>
+              {filterMaybes(bottomLinks?.rightLinks).map((navItem) => (
+                <div key={navItem._key} className="font-size-10 md:ml-8">
+                  <Link link={navItem.link}>{navItem.label}</Link>
                 </div>
               ))}
             </div>
           </div>
-          <div className={cn(FooterQRCode)}>
-            <span>QR Code placeholder</span>
-          </div>
         </div>
-        <div className={cn(FooterBottomLinks)}>
-          <div className={cn(FooterBottomNavGroup)}>
-            {filterMaybes(bottomLinks?.leftLinks).map((navItem) => (
-              <div key={navItem._key} className="font-size-10 md:mr-8">
-                <Link link={navItem.link}>{navItem.label}</Link>
-              </div>
-            ))}
-          </div>
-          <div className={cn(FooterBottomNavGroup)}>
-            {filterMaybes(bottomLinks?.rightLinks).map((navItem) => (
-              <div key={navItem._key} className="font-size-10 md:ml-8">
-                <Link link={navItem.link}>{navItem.label}</Link>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    </footer>
+      </footer>
+    </Theme>
   );
 };
