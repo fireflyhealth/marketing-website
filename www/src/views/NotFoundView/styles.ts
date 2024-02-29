eb39fd509e4613a375ac103989d9659397a45941
Bryan Washington <beewash92@gmail.com>
2024-02-29 11:43:26 -0500

    feat: update decorative image styling

--- www/src/views/NotFoundView/styles.ts
+++ www/src/views/NotFoundView/styles.ts
@@ -2,10 +2,10 @@ export const Wrapper = ['relative'];
 
 export const DecorativeImageWrapper = [
   'w-full block',
-  'lg:absolute lg:left-0 lg:top-2/4',
+  'lg:absolute lg:w-full lg:h-full lg:left-0 lg:top-[15%]',
 ];
 
 export const Image = [
   'relative w-[321px] h-[321px] mx-auto',
-  'lg:m-0 lg:h-[27vh]',
+  'lg:m-0 lg:w-[25vw] lg:h-[50vh]',
 ];
