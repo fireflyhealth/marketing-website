fb4dbdeeb965eec1eb324675dea6dc4a1235a5c9
Joseph <joseph@good-idea.studio>
2024-03-05 20:54:09 -0800

    feat: add Blog View content blocks & featured article

--- www/src/views/Blog/BlogPageView.tsx
+++ www/src/views/Blog/BlogPageView.tsx
@@ -1,15 +1,31 @@
 import React, { FC } from 'react';
 import { Blog } from '@/types/sanity';
 import { HeaderArea } from '@/components/headerContentBlocks/HeaderArea';
+import { ContentArea } from '@/components/contentBlocks/ContentArea';
+import { BlogPageFeaturedArticle } from './BlogPageFeaturedArticle';
 
 type BlogPageViewProps = {
   blog: Blog;
 };
 
 export const BlogPageView: FC<BlogPageViewProps> = ({ blog }) => {
+  const {
+    header,
+    articles,
+    featuredArticle,
+    contentArea,
+    blogArticleTagGroups,
+    articleLayout,
+  } = blog;
   return (
     <div>
       <HeaderArea block={blog.header} />
+      {contentArea ? <ContentArea blocks={contentArea} /> : null}
+      {featuredArticle ? (
+        <div>
+          <BlogPageFeaturedArticle article={featuredArticle} />
+        </div>
+      ) : null}
     </div>
   );
 };
---
+++ www/src/views/Blog/styles.ts
@@ -0,0 +1,15 @@
+/* Featured Article */
+export const FeaturedArticleContainer = ['flex', 'md:flex-row'];
+export const FeaturedArticleImageContainer = [
+  'lg:w-[calc(50%_+_3rem)] lg:mt-12',
+];
+export const FeaturedArticleTextContainer = [
+  'mt-[-20px]',
+  'lg:w-[calc(50%_+_3rem)] lg:flex-grow-1 lg:mt-0 lg:ml-[-48px]',
+];
+
+export const FeaturedArticleTextInner = [
+  'theme-bg-color rounded-2xl mix-blend-multiply',
+  'py-12 px-8',
+  'md:py-24 md:px-24',
+];
