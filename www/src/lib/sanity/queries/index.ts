0cae7259b03a25ab83970ef4b7edbc56531f12dc
Joseph <joseph@good-idea.studio>
2024-03-05 20:02:34 -0800

    feat: add updated blog & article fields to types & groq

--- www/src/lib/sanity/queries/index.ts
+++ www/src/lib/sanity/queries/index.ts
@@ -132,3 +132,32 @@ export const blogFragment = `
   },
   articleLayout
 `;
+
+export const blogFragment = `
+  _id,
+  title,
+  slug,
+  navigationOverrides {${navigationOverridesFragment}},
+  'header': header[] {${headerBlockFragment}}[0],
+  metadataFragment{${metadataFragment}},
+  'articles': *[_type == "blogArticle" && category->_id == ^._id]{
+    ${blogArticleLinkDataFragment}
+  },
+  featuredArticle->{
+    ${blogArticleLinkDataFragment}
+  },
+  contentArea[]{
+    ${contentBlockFragment}
+  },
+  allArticlesLabel,
+  blogArticleTagGroups[]{
+    _type,
+    _key,
+    title,
+    tag->{
+      title,
+      slug
+    }
+  },
+  articleLayout
+`;
