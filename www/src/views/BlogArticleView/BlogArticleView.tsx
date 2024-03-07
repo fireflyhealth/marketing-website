3644ea4683f6d659d89ac463c45830d837ddb76d
Sohee Lim <limsohee1002@gmail.com>
2024-03-07 18:16:53 +0900

    feat: add article rich text style on article page

--- www/src/views/BlogArticleView/BlogArticleView.tsx
+++ www/src/views/BlogArticleView/BlogArticleView.tsx
@@ -1,6 +1,8 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
 import { BlogArticle } from '@/types/sanity';
 import { ArticleHeader } from '@/components/ArticleHeader';
+import { RichText } from '@/components/RichText';
 
 type BlogArticleViewProps = {
   article: BlogArticle;
@@ -8,8 +10,25 @@ type BlogArticleViewProps = {
 
 export const BlogArticleView: FC<BlogArticleViewProps> = ({ article }) => {
   return (
-    <div>
+    <div className={cn('BlogArticleView px-4 md:px-8 lg:px-12')}>
+      {/* TODO replace this with artible header */}
       <ArticleHeader articleHeader={article} />
+      <article className={cn('flex flex-col items-center')}>
+        {article.deck && (
+          <RichText
+            className={cn('font-trust w-full md:w-2/3 lg:w-5/6 lg:mb-16')}
+            content={article.deck}
+            fontSize="font-size-6--quote"
+            textColor="text-sienna"
+          />
+        )}
+        {article.content && (
+          <RichText
+            className={cn('ArticleRichText w-full md:w-2/3 lg:w-4/6')}
+            content={article.content}
+          />
+        )}
+      </article>
     </div>
   );
 };
