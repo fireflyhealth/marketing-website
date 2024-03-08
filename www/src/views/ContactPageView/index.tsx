e930da41558e3f0697780d526faee8e50ea8ccbb
Bryan Washington <beewash92@gmail.com>
2024-03-08 16:42:58 -0500

    feat: setup contact page

--- www/src/views/ContactPageView/index.tsx
+++ www/src/views/ContactPageView/index.tsx
@@ -1,10 +1,61 @@
 import React, { FC } from 'react';
+import cn from 'classnames';
 import { ContactPage } from '@/types/sanity';
+import { RichText } from '@/components/RichText';
+import { SanityImage } from '@/atoms/Image/SanityImage';
+import { HubspotForm } from '@/components/HubspotForm';
+import {
+  Wrapper,
+  HeaderWrapper,
+  Header,
+  PageTitle,
+  ContactForm,
+  BackgroundColor,
+  BackgroundImage,
+} from './styles';
 
 type ContactPageViewProps = {
   contactPage: ContactPage;
 };
 
 export const ContactPageView: FC<ContactPageViewProps> = ({ contactPage }) => {
-  return <div>{contactPage.title}</div>;
+  const {
+    pageTitle,
+    pageDescription,
+    contactForm,
+    backgroundColor,
+    backgroundImage,
+  } = contactPage;
+
+  return (
+    <div className={cn(Wrapper)}>
+      <div className={cn(HeaderWrapper)}>
+        <div className={cn(Header)}>
+          <h1 className={cn(PageTitle)}>{pageTitle}</h1>
+          <RichText
+            content={pageDescription}
+            fontSize="font-size-8 font-roobert theme-text-color-primary"
+          />
+        </div>
+        <div className={cn(ContactForm)}>
+          <HubspotForm formId={contactForm.formId} />
+        </div>
+      </div>
+      {backgroundColor && (
+        <div
+          className={cn(BackgroundColor)}
+          style={{ backgroundColor: `${backgroundColor}` }}
+        />
+      )}
+      {backgroundImage && (
+        <div className={cn(BackgroundImage)}>
+          <SanityImage
+            image={backgroundImage}
+            sizes={['100vw']}
+            className="w-full"
+          />
+        </div>
+      )}
+    </div>
+  );
 };
---
+++ www/src/views/ContactPageView/styles.ts
@@ -0,0 +1,21 @@
+export const Wrapper = ['relative min-h-[50vh] p-4', 'md:p-12'];
+
+export const HeaderWrapper = [
+  'w-full flex flex-col text-center space-y-[83px]',
+  'lg:flex-row lg:space-y-0 lg:space-x-12',
+];
+
+export const Header = ['lg:w-1/2 lg:my-28 lg:mx-[82px]'];
+
+export const PageTitle = [
+  'font-size-3 font-trust theme-text-color-primary mb-8',
+];
+
+export const ContactForm = ['w-full', 'lg:w-1/2'];
+
+export const BackgroundColor = [
+  '!z-[-2] !-top-[56px] full-width-background',
+  'lg:!-top-[82px]',
+];
+
+export const BackgroundImage = ['lg:!top-1/3 lg:full-width-background'];
