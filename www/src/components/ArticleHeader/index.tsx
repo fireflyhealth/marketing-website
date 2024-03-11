d8918a269ee12092dd0665e0efb2d7f328d7cd2b
Sohee Lim <limsohee1002@gmail.com>
2024-03-11 15:23:52 +0900

    feat: render article header with image

--- www/src/components/ArticleHeader/index.tsx
+++ www/src/components/ArticleHeader/index.tsx
@@ -2,6 +2,17 @@ import { FC } from 'react';
 import { format, isEqual } from 'date-fns';
 import cn from 'classnames';
 import * as SanityTypes from '@/types/sanity';
+import { SanityImage } from '@/atoms/Image/SanityImage';
+import {
+  Wrapper,
+  TextContentWrapper,
+  Tag,
+  Title,
+  Author,
+  DateWrapper,
+  Date as DateStyle,
+  Image,
+} from './styles';
 
 type Props = {
   articleHeader: SanityTypes.BlogArticle;
@@ -16,6 +27,7 @@ export const ArticleHeader: FC<Props> = ({ articleHeader }) => {
     _createdAt,
     updatedDate,
     _updatedAt,
+    articleImage,
   } = articleHeader;
   const selectedPublishedDate = publishDate || _createdAt;
   let selectedUpdatedDate: string | null = updatedDate || _updatedAt;
@@ -27,49 +39,40 @@ export const ArticleHeader: FC<Props> = ({ articleHeader }) => {
     : selectedUpdatedDate;
 
   return (
-    <div
-      className={cn(
-        'ArticleHeader container-padding py-12 lg:py-24 mx-auto flex flex-row items-center justify-center',
-      )}
-    >
+    <div className={cn(Wrapper)}>
       <div
-        className={cn(
-          'ArticleHeader__text-content-wrapper max-w-[960px] flex flex-col items-center justify-center text-center text-midnight',
-        )}
+        className={cn(TextContentWrapper, {
+          'lg:w-1/2': articleImage,
+        })}
       >
         {tags?.length && (
-          <div
-            className={cn(
-              'mb-12 border-grey-medium border pt-2 pb-2.5 px-5 rounded-[33px]',
-            )}
-          >
+          <div className={cn(Tag)}>
             <p className={cn()}>{tags[0].title}</p>
           </div>
         )}
         <h1 className={cn('font-size-3 font-trust')}>{title}</h1>
-        <div
-          className={cn(
-            'mt-12 font-size-8 font-roobert flex flex-col items-center',
-          )}
-        >
-          {authorName && <p className={cn('mb-2')}>by {authorName}</p>}
-          <div
-            className={cn(
-              'text-grey-darker flex flex-row flex-wrap items-center justify-center',
-            )}
-          >
-            <p className={cn('mx-3')}>
+        <div className={cn(Title)}>
+          {authorName && <p className={cn(Author)}>by {authorName}</p>}
+          <div className={cn(DateWrapper)}>
+            <p className={cn(DateStyle)}>
               Published{' '}
               {format(new Date(selectedPublishedDate), 'MMMM d, yyyy')}
             </p>
             {selectedUpdatedDate && (
-              <p className={cn('mx-3')}>
+              <p className={cn(DateStyle)}>
                 Updated {format(new Date(selectedUpdatedDate), 'MMMM d, yyyy')}
               </p>
             )}
           </div>
         </div>
       </div>
+      {articleImage && (
+        <SanityImage
+          className={cn(Image)}
+          image={articleImage}
+          sizes={['100vw', '50vw']}
+        />
+      )}
     </div>
   );
 };
---
+++ www/src/components/ArticleHeader/styles.ts
@@ -0,0 +1,25 @@
+export const Wrapper = [
+  'ArticleHeader container-padding py-12 lg:py-24 mx-auto flex flex-col lg:flex-row items-center justify-center lg:space-x-12',
+];
+
+export const TextContentWrapper = [
+  'ArticleHeader__text-content-wrapper max-w-[960px] flex flex-col items-center justify-center text-center text-midnigh',
+];
+
+export const Tag = [
+  'mb-12 border-grey-medium border pt-2 pb-2.5 px-5 rounded-[33px]',
+];
+
+export const Title = [
+  'mt-12 font-size-8 font-roobert flex flex-col items-center',
+];
+
+export const Author = ['mb-2'];
+
+export const DateWrapper = [
+  'text-grey-darker flex flex-row flex-wrap items-center justify-center',
+];
+
+export const Date = ['mx-3'];
+
+export const Image = ['md:w-2/3 lg:w-1/2 mt-12 lg:mt-0'];
