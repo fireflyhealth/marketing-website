0e0780204e0a2b227daec6258ff30f30fe265e9b
Bryan Washington <beewash92@gmail.com>
2024-03-12 14:27:55 -0400

    feat: refactor bar graph sizing logic

--- www/src/components/BarGraph/index.tsx
+++ www/src/components/BarGraph/index.tsx
@@ -1,4 +1,4 @@
-import { FC, useEffect } from 'react';
+import { FC, useEffect, useRef } from 'react';
 import cn from 'classnames';
 import * as SanityTypes from '@/types/sanity';
 import {
@@ -16,44 +16,57 @@ type Props = {
 
 export const BarGraph: FC<Props> = ({ barGraph }) => {
   const { barOne, barTwo } = barGraph;
-  const barTwoSize = Math.round((barTwo.unit / barOne.unit) * 100);
+  const barTwoSize = Math.round(350 * (barTwo.unit / barOne.unit));
+  const barRef = useRef<HTMLDivElement>(null);
 
   // Tailwind compiles styles at compile time
   // so we handle dynamic sizing with root variables.
   useEffect(() => {
     document.documentElement.style.setProperty(
       '--bar-graph-height',
-      `${barTwoSize}%`,
-    );
-    document.documentElement.style.setProperty(
-      '--bar-graph-width',
-      `${barTwoSize}%`,
+      `${barTwoSize}px`,
     );
   }, [barTwoSize]);
+
+  useEffect(() => {
+    if (!barRef.current) return;
+
+    const barOneWidth = barRef.current.clientWidth;
+
+    const barTwoWidth = Math.round(barOneWidth * (barTwo.unit / barOne.unit));
+
+    document.documentElement.style.setProperty(
+      '--bar-graph-width',
+      `${barTwoWidth}px`,
+    );
+  }, [barRef, barOne, barTwo]);
   return (
     <div className={cn(Wrapper)}>
       <div className={cn(BarItem)}>
-        <div className={cn(BarOneUnit, 'hidden md:block md:mb-3')}>
+        <div className={cn(BarOneUnit, 'hidden lg:block lg:mb-3')}>
           {barOne.unit}%
         </div>
-        <div className={cn(Bar, 'w-full h-full', 'bg-yellow')}>
+        <div
+          ref={barRef}
+          className={cn(Bar, 'bg-yellow w-full h-full', 'lg:h-[350px]')}
+        >
           <p className={cn(Description)}>{barOne.description}</p>
-          <div className={cn(BarOneUnit, 'md:hidden')}>{barOne.unit}%</div>
+          <div className={cn(BarOneUnit, 'lg:hidden')}>{barOne.unit}%</div>
         </div>
       </div>
       <div className={cn(BarItem)}>
-        <div className={cn(BarTwoUnit, 'hidden md:block md:mb-3')}>
+        <div className={cn(BarTwoUnit, 'hidden lg:block lg:mb-3')}>
           {barTwo.unit}%
         </div>
         <div
           className={cn(
             Bar,
-            `w-bar-graph-width md:w-full md:h-bar-graph-height`,
+            `min-w-[30%] w-bar-graph-width lg:w-full lg:h-bar-graph-height lg:min-h-[56px]`,
             'bg-grey-medium',
           )}
         >
           <p className={cn(Description, 'opacity-70')}>{barTwo.description}</p>
-          <div className={cn(BarTwoUnit, 'md:hidden')}>{barTwo.unit}%</div>
+          <div className={cn(BarTwoUnit, 'lg:hidden')}>{barTwo.unit}%</div>
         </div>
       </div>
     </div>
--- www/src/components/BarGraph/styles.ts
+++ www/src/components/BarGraph/styles.ts
@@ -1,19 +1,19 @@
 export const Wrapper = [
-  'w-full h-[316px] py-6 flex flex-col space-y-3',
-  'md:h-[400px] md:flex-row md:space-y-0 md:space-x-2.5',
+  'w-full py-6 flex flex-col space-y-3',
+  'lg:flex-row lg:items-end lg:space-y-0 lg:space-x-2.5',
 ];
 
 export const BarItem = [
   'h-1/2',
-  'md:w-full md:h-full md:flex md:flex-col md:justify-end',
+  'lg:w-full lg:h-full lg:flex lg:flex-col lg:justify-end',
 ];
 
-export const BarOneUnit = ['font-size-6 theme-color-primary md:text-center'];
+export const BarOneUnit = ['font-size-6 theme-color-primary lg:text-center'];
 
-export const BarTwoUnit = ['font-size-7 theme-color-secondary md:text-center'];
+export const BarTwoUnit = ['font-size-7 theme-color-secondary lg:text-center'];
 
 export const Bar = [
   'flex flex-col justify-between h-full rounded-md px-3.5 py-3',
 ];
 
-export const Description = ['font-size-8 text-black md:text-center'];
+export const Description = ['font-size-8 text-black lg:text-center'];
