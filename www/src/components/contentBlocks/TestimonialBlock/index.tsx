1709d07fe3f83b2a1b9b22716cafb10f1d8f7cab
Sohee Lim <limsohee1002@gmail.com>
2024-03-15 16:10:32 +0900

    fix: adjust testimonial block spacing

--- www/src/components/contentBlocks/TestimonialBlock/index.tsx
+++ www/src/components/contentBlocks/TestimonialBlock/index.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react';
+import cn from 'classnames';
 import { Carousel } from '@/components/Carousel';
 import { TestimonialBlock as TestimonialBlockType } from '@/types/sanity';
 import { ContentBlockWrapper } from '../ContentBlockWrapper';
@@ -13,14 +14,16 @@ export const TestimonialBlock: FC<Props> = ({ testimonialBlock }) => {
 
   return (
     <ContentBlockWrapper header={header} id={subnav?.contentBlockId}>
-      <Carousel>
-        {testimonials.map((testimonial) => (
-          <TestimonialItem
-            key={testimonial._key}
-            testimonialItem={testimonial}
-          />
-        ))}
-      </Carousel>
+      <div className={cn('mt-8 md:mt-0')}>
+        <Carousel>
+          {testimonials.map((testimonial) => (
+            <TestimonialItem
+              key={testimonial._key}
+              testimonialItem={testimonial}
+            />
+          ))}
+        </Carousel>
+      </div>
     </ContentBlockWrapper>
   );
 };
