07fd91934f1386361bddbe34918e4819ba84b33d
Sohee Lim <limsohee1002@gmail.com>
2024-03-21 18:27:14 +0900

    fix: add faq block animation

--- www/src/atoms/Accordion/index.tsx
+++ www/src/atoms/Accordion/index.tsx
@@ -1,6 +1,7 @@
 import React, { FC, ReactNode, useState, useEffect, useRef } from 'react';
 import cn from 'classnames';
 import { SimpleIcon } from '@/svgs/SimpleIcon';
+import { useWindowDimensions } from '@/hooks/useWindowDimensions';
 import { TitleWrapper } from './styles';
 
 type AccordionProps = {
@@ -11,8 +12,18 @@ type AccordionProps = {
 
 export const Accordion: FC<AccordionProps> = ({ title, children, active }) => {
   const accordionRef = useRef<HTMLDivElement>(null);
-
+  const innerContentRef = useRef<HTMLDivElement>(null);
+  const windowDimensions = useWindowDimensions();
   const [isOpen, setIsOpen] = useState(false);
+  const [expandedContentHeight, setExpandedContentHeight] = useState<
+    string | number
+  >(
+    /* Set the initial state to 'auto' if isOpen is true upon first render.
+     * The effect below will set this to a number (which will match the natural height)
+     * when it first runs. */
+    isOpen ? 'auto' : 0,
+  );
+
   const toggleOpen = () => setIsOpen((value) => !value);
 
   useEffect(() => {
@@ -26,6 +37,31 @@ export const Accordion: FC<AccordionProps> = ({ title, children, active }) => {
       }
     }
   }, [active, accordionRef.current]);
+
+  useEffect(() => {
+    /* Calculate and set the inner content height whenever the element
+     * is expanded or when the window is resized. */
+    const innerContentRefCurrent = innerContentRef.current;
+
+    if (isOpen) {
+      if (!innerContentRefCurrent) return;
+      const innerContentHeight = innerContentRefCurrent.offsetHeight;
+      setExpandedContentHeight(innerContentHeight);
+
+      /* Set tabindex to 0 for all links in the expanded content to be focusable */
+      innerContentRefCurrent.querySelectorAll('a').forEach((link) => {
+        link.setAttribute('tabindex', '0');
+      });
+    } else {
+      setExpandedContentHeight(0);
+
+      if (!innerContentRefCurrent) return;
+      /* Set tabindex to -1 for all links in the expanded content to not be focusable */
+      innerContentRefCurrent.querySelectorAll('a').forEach((link) => {
+        link.setAttribute('tabindex', '-1');
+      });
+    }
+  }, [isOpen, windowDimensions]);
 
   return (
     <div ref={accordionRef}>
@@ -43,10 +79,26 @@ export const Accordion: FC<AccordionProps> = ({ title, children, active }) => {
         </div>
       </button>
       <div
-        aria-hidden={!isOpen}
-        className={isOpen ? 'block' : 'opacity-0 h-0 pointer-events-none'}
+        style={{
+          transition: 'height 0.3s ease',
+          /* We cannot transition between height: 0 and height: auto,
+           * so we need to set an explicit number height for the animation.
+           * The effect above will calculate the height of the inner content
+           * when the item is expanded, and then apply that number to this
+           * wrapping div, animating from 0px to <some-number>px. */
+          height: expandedContentHeight,
+        }}
       >
-        <div className="pt-3 pr-7">{children}</div>
+        <div
+          ref={innerContentRef}
+          aria-hidden={!isOpen}
+          className={cn(
+            'transition-all block',
+            isOpen ? '' : 'opacity-0 pointer-events-none',
+          )}
+        >
+          <div className="pt-3 pr-7">{children}</div>
+        </div>
       </div>
     </div>
   );
