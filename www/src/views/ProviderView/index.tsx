243ee373cad4ce73a94979efce7e6b00967d97dd
Bryan Washington <beewash92@gmail.com>
2024-03-22 16:36:41 -0400

    feat: style provider header

--- www/src/views/ProviderView/index.tsx
+++ www/src/views/ProviderView/index.tsx
@@ -1,8 +1,22 @@
 import React, { FC } from 'react';
-
+import cn from 'classnames';
 import { LinkableDocumentData, Practitioner } from '@/types/sanity';
 import { ContentArea } from '@/components/contentBlocks/ContentArea';
 import { Link } from '@/atoms/Link';
+import { ColorTheme, Theme } from '@/components/Theme';
+import { RichText } from '@/components/RichText';
+import { CTA } from '@/components/CTA';
+import { SanityImage } from '@/atoms/Image/SanityImage';
+import { BrandedIcon } from '@/svgs/BrandedIcon';
+import {
+  HeaderTextWrapper,
+  HeaderSubtitle,
+  HeaderTitle,
+  Qualifications,
+  QualificationSection,
+  Institution,
+  Language,
+} from './styles';
 
 export type ProviderPageViewProps = {
   provider: Practitioner;
@@ -13,12 +27,110 @@ export const ProviderPageView: FC<ProviderPageViewProps> = ({
   provider,
   allProvidersBackLink,
 }) => {
+  const {
+    headerBgThemeColor,
+    title,
+    name,
+    blurb,
+    education,
+    languagesSpoken,
+    headshot,
+  } = provider;
   return (
     <div>
-      <div>
-        <Link link={allProvidersBackLink}>{`<All Providers`}</Link>
+      <div className="Provider__Header">
+        <div>
+          <Link
+            link={allProvidersBackLink}
+            className="simple-text-link border-black"
+          >{`< All Providers`}</Link>
+        </div>
+        <div>
+          <div
+            className={cn(
+              'TwoUpBlock TwoUpBlock--layout-overlap-50-50 TwoUpBlock--mobileReverse',
+            )}
+          >
+            <div
+              className={cn(
+                'TwoUpBlock__child',
+                `TwoUpBlock__child--richTextChildBlock`,
+              )}
+            >
+              <Theme theme={headerBgThemeColor || ColorTheme.Midnight}>
+                <div className={cn(HeaderTextWrapper)}>
+                  <p className={cn(HeaderSubtitle)}>{title}</p>
+                  <h1 className={cn(HeaderTitle)}>{name}</h1>
+                  <RichText
+                    content={blurb}
+                    fontSize="font-size-8 font-roobert"
+                  />
+                  <div className={cn(Qualifications)}>
+                    {education && (
+                      <div className={cn(QualificationSection)}>
+                        <BrandedIcon
+                          type="education"
+                          wrapperStyles="w-6 h-6"
+                          iconStyles="text-color-primary"
+                        />
+                        {education.map((institution, index) => (
+                          <span
+                            key={institution._key}
+                            className={cn(Institution)}
+                          >
+                            {institution.name}
+                            {education.length > 0 &&
+                              index < education.length - 1 &&
+                              ` ,`}
+                          </span>
+                        ))}
+                      </div>
+                    )}
+                    {languagesSpoken && (
+                      <div className={cn(QualificationSection)}>
+                        <BrandedIcon
+                          type="languages"
+                          wrapperStyles="w-6 h-6"
+                          iconStyles="text-color-primary"
+                        />
+                        {languagesSpoken.map((language, index) => (
+                          <span key={index} className={cn(Language)}>
+                            {language}
+                            {languagesSpoken.length > 0 &&
+                              index < languagesSpoken.length - 1 &&
+                              `, `}
+                          </span>
+                        ))}
+                      </div>
+                    )}
+                  </div>
+                  {/* Add 'Accepting Patients` status */}
+                  {/* Add CTA */}
+                </div>
+              </Theme>
+            </div>
+            <div
+              className={cn(
+                'TwoUpBlock__child',
+                `TwoUpBlock__child--imageChildBlock`,
+              )}
+            >
+              <Theme theme={headerBgThemeColor || ColorTheme.Midnight}>
+                <div className={cn('theme-bg-color rounded-2xl')}>
+                  {headshot && (
+                    <SanityImage
+                      sizes={['100vw', '50vw']}
+                      image={headshot}
+                      rounded={false}
+                      className={cn('rounded-2xl')}
+                    />
+                  )}
+                </div>
+              </Theme>
+            </div>
+          </div>
+        </div>
       </div>
-      <div>{provider.name}</div>
       {provider.contentArea && <ContentArea blocks={provider.contentArea} />}
     </div>
   );
---
+++ www/src/views/ProviderView/styles.tsx
@@ -0,0 +1,17 @@
+export const HeaderTextWrapper = [
+  'theme-bg-color rounded-2xl space-y-8 lg:space-y-10 text-center',
+];
+
+export const HeaderSubtitle = ['font-size-8--cta font-roobert'];
+
+export const HeaderTitle = ['font-size-3 font-trust'];
+
+export const Qualifications = [
+  'flex flex-row flexwrap justify-center space-x-6',
+];
+
+export const QualificationSection = ['flex flex-row align-center space-x-2'];
+
+export const Institution = [''];
+
+export const Language = [''];
