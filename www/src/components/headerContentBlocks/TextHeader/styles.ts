75c01cbf46d54bf41ca5f847ab509b13108756a2
Sohee Lim <limsohee1002@gmail.com>
2024-04-08 19:45:26 -0400

    fix: text header

--- www/src/components/headerContentBlocks/TextHeader/styles.ts
+++ www/src/components/headerContentBlocks/TextHeader/styles.ts
@@ -4,12 +4,12 @@ export const Glow = [
   'absolute -z-50 opacity-90 blur-3xl -top-[30%] left-[50%] -translate-x-[50%] h-full w-[120vw] bg-yellow-light rounded-[674.12px]',
 ];
 
-export const Wrapper = ['lg:py-12'];
+export const Wrapper = ['lg:py-grid-margin-lg'];
 
 export const WrapperInner = [
-  'px-6 py-16',
+  'py-16',
   'flex flex-col justify-center',
-  'md:px-24 md:py-40',
+  'md:px-[60px] md:py-40',
   'lg:px-16 lg:pt-40 lg:pb-12 max-w-[1216px] mx-auto',
 ];
 
