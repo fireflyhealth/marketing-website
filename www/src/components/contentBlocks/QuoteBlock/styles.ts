9c04a9b9ab80bd1ab8fd13b24ce9c4761ad378dc
Sohee Lim <limsohee1002@gmail.com>
2024-04-09 19:29:21 -0400

    fix: quote padding

--- www/src/components/contentBlocks/QuoteBlock/styles.ts
+++ www/src/components/contentBlocks/QuoteBlock/styles.ts
@@ -2,7 +2,7 @@ export const Wrapper = [
   'container-padding-bleed--margin-only',
   'theme-bg-color rounded-2xl px-grid-margin-sm py-16 space-y-6',
   'md:px-grid-margin-lg md:py-24 md:space-y-[43px]',
-  'lg:px-[164p] lg:py-40 md:space-y-12',
+  'lg:px-[164p] lg:py-20 md:space-y-12',
 ];
 
 export const CTAWrapper = ['w-full', 'md:w-fit'];
