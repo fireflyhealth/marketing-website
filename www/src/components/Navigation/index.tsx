99fdcf30f7a9caf65fdfbb7dfcce7159f817b897
Bryan Washington <beewash92@gmail.com>
2024-04-15 18:25:01 -0400

    feat: add keydown listener to close nav cta, lock scroll when nav cta is open

--- www/src/components/Navigation/index.tsx
+++ www/src/components/Navigation/index.tsx
@@ -2,6 +2,7 @@ import { FC, useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { Navigation as NavigationType, DoubleCta } from '@/types/sanity';
 import { useUIProvider } from '@/context/UIProvider';
+import { useBodyLock } from '@/hooks/useBodyLock';
 import { MobileNav } from './MobileNav';
 import { DesktopNav } from './DesktopNav';
 
@@ -16,7 +17,8 @@ export const Navigation: FC<Props> = ({
   showNavCTA,
   globalDoubleNav,
 }) => {
-  const { setMobileNavOpen, setGetStartedOpen } = useUIProvider();
+  const { setMobileNavOpen, getStartedOpen, setGetStartedOpen } =
+    useUIProvider();
   const router = useRouter();
 
   // close globalNav and globalNavDropdown on route change
@@ -30,6 +32,27 @@ export const Navigation: FC<Props> = ({
       router.events.off('routeChangeStart', closeNavDropdowns);
     };
   }, [router, setMobileNavOpen, setGetStartedOpen]);
+
+  useEffect(() => {
+    // Handle keyboard events (open/close dropdown, select option)
+    const handleKeyDown = (event: KeyboardEvent) => {
+      const { key } = event;
+      if (key === 'Escape') {
+        setMobileNavOpen(false);
+        setGetStartedOpen(false);
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [getStartedOpen, setGetStartedOpen, setMobileNavOpen]);
+
+  // Lock body scroll when the secondary nav (navCTA/'Get Started') is open
+  useBodyLock(getStartedOpen);
+
   return (
     <>
       <MobileNav
