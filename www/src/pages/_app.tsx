cc447e233490f18b9e3a72353e4061e0e9f2bd7b
Sohee Lim <limsohee1002@gmail.com>
2024-04-23 13:02:24 -0400

    fix: cleanup

--- www/src/pages/_app.tsx
+++ www/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { AppProps } from 'next/app';
+import Script from 'next/script';
 import { HubspotProvider } from 'next-hubspot';
 import { ABCookieManager } from '@/utils/storage';
 import { AnnouncementBanner } from '@/components/AnnouncementBanner';
@@ -43,6 +44,17 @@ export default function App({ Component, pageProps: allPageProps }: Props) {
   return (
     <>
       <DefaultMetadata metadata={siteSettings.defaultMetadata} />
+      <Script
+        id="gtm-script"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-PDTZ29Q7');`,
+        }}
+      />
       {/* We control overflow from the highest parent container outside of all sitewide padding and margin.
           For mobile devices, overflow is set to 'clip' as opposed to 'hidden' to keep all content within width, padding and margin boundaries
           while allowing content that should bleed off the page to do so without causing any layout shifts.
