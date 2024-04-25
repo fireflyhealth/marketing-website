b9100e9cf115adc1d4356a851944c0cbef27e34e
Bryan Washington <beewash92@gmail.com>
2024-04-25 16:43:53 -0400

    fix: remove padding-top from tabs component

--- www/src/components/Tabs/styles.ts
+++ www/src/components/Tabs/styles.ts
@@ -1,6 +1,6 @@
 export const TabLabels = [
-  'pt-12 px-0 pb-[14px] flex flex-row whitespace-nowrap overflow-x-scroll hide-scrollbar',
-  'md:py-12',
+  'px-0 pb-[14px] flex flex-row whitespace-nowrap overflow-x-scroll hide-scrollbar',
+  'md:pb-12',
 ];
 export const TabLabel = [
   'font-size-6 font-trust mr-6 font-light',
